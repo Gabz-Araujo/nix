10abdb03822ca5e388507fd1bbb20333074157f4
Gabz-Araujo <g.cardoso.araujo@gmail.com>
2024-11-06 02:46:55 +0000

    Initial nix config

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Darwin Config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, nix-homebrew, ... }:
+  {
+    darwinConfigurations = {
+        "MacBook-Pro-de-Tiba-2" = nix-darwin.lib.darwinSystem {
+        system = "aaarch64-darwin";
+        modules = [
+          ./configuration.nix
+          home-manager.darwinModules.home-manager
+            {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users."GabrielAraujo" = import ./home.nix;
+            }
+        nix-homebrew.darwinModules.nix-homebrew
+            {
+            nix-homebrew = {
+              enable = true;
+              enableRosetta = true;
+              user = "GabrielAraujo";
+              autoMigrate = true;
+            };
+          }
+        ];
+        };
+      };
+  };
+}
